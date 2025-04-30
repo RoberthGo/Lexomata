c9eac5e00f2fac66ec9216eb0abf6bad0068525c
Jesús Samuel Zavala Ayala <jsamuelzavalay@gmail.com>
2025-04-29 21:41:26 -0600

    feat: implement canvas click event and draw circle function

---
+++ lexomata/js/utils/addNode.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.addEventListener('click', (event) => {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    console.log(`Clicked at: (${x}, ${y})`);
+    drawCircle(x, y);
+});
+
+function drawCircle(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI * 2);
+    ctx.fillStyle = 'lightblue';
+    ctx.fill();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+}
