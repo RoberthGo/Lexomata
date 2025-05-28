bda3dedf7c40e102da2d2e7b17c841b1415404cb
Jesús Samuel Zavala Ayala <jsamuelzavalay@gmail.com>
2025-05-28 12:34:57 -0600

    refactor: separate canvas click event handler and improve state management

--- lexomata/js/utils/addNode.js
+++ lexomata/js/utils/addNode.js
@@ -1,14 +1,15 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
-canvas.addEventListener('click', (event) => {
+const States = [];
+let index=0;
+function createState(event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    console.log(`Clicked at: (${x}, ${y})`);
     drawCircle(x, y);
-});
-
+    States[index] = new State("q" + index.toString());
+    index++;
+}
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, Math.PI * 2);
@@ -18,3 +19,4 @@ function drawCircle(x, y) {
     ctx.lineWidth = 2;
     ctx.stroke();
 }
+canvas.addEventListener('click', createState);
