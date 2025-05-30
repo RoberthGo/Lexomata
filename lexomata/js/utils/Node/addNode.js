0ea61b8a027c22cda4c5c38b0bf7564ba655c522
RoberthGo <robeg7183@gmail.com>
2025-05-30 00:56:16 -0600

    Import drawNode function

--- lexomata/js/utils/Node/addNode.js
+++ lexomata/js/utils/Node/addNode.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const States = [];
-let index=0;
+let index = 0;
+
 function createState(event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
@@ -10,6 +11,7 @@ function createState(event) {
     States[index] = new State("q" + index.toString());
     index++;
 }
+
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, Math.PI * 2);
@@ -19,4 +21,5 @@ function drawCircle(x, y) {
     ctx.lineWidth = 2;
     ctx.stroke();
 }
+
 canvas.addEventListener('click', createState);
