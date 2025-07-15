83d4084c02efe4ba562e044340e309245f0a7fec
Jesús Samuel Zavala Ayala <jsamuelzavalay@gmail.com>
2025-07-15 14:28:45 -0600

    Fix: addEdge not detecting the states correctly, update line width in drawEdge function and adjust edge drawing coordinates in addEdge function

--- lexomata/js/utils/Draw/drawEdge.js
+++ lexomata/js/utils/Draw/drawEdge.js
@@ -1,7 +1,7 @@
 function drawEdge(x1, y1, x2, y2) {
     ctx.save();
     ctx.strokeStyle = '#222'; // dark color
-    ctx.lineWidth = 2;
+    ctx.lineWidth = 3;
     ctx.beginPath();
     ctx.moveTo(x1, y1);
     ctx.lineTo(x2, y2);
--- lexomata/js/utils/canvas/addEdge.js
+++ lexomata/js/utils/canvas/addEdge.js
@@ -18,13 +18,13 @@ const rect = canvas.getBoundingClientRect();
     console.log(indexState);
     if(indexState!=-1){
         if(isSelected){
+            console.log(states[OriginState].x);
             // Si ya hay un estado seleccionado, se añade la arista
             // desde el estado anteriormente seleccionado al estado actual
             states[OriginState].insertEdge(new Edge(OriginState, states[indexState]));
-            drawEdge(0,0,100,100)
+            drawEdge(states[OriginState].x,states[OriginState].y,states[indexState].x,states[indexState].y)
             OriginState=null;
             isSelected=false;
-            console.log(states[OriginState]);
         }else{
             // Si no hay un estado seleccionado, se guarda el estado
             // actual como el origen
